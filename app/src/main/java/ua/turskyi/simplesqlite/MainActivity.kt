d1b37c227c0aed46828d60175aa7566522658e77
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-03-02 16:40:13 +0200

    update and delete

--- app/src/main/java/ua/turskyi/simplesqlite/MainActivity.kt
+++ app/src/main/java/ua/turskyi/simplesqlite/MainActivity.kt
@@ -2,7 +2,6 @@ package ua.turskyi.simplesqlite
 
 import android.content.ContentValues
 import android.content.Context
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
@@ -27,19 +26,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
         btnAdd.setOnClickListener(this)
         btnRead.setOnClickListener(this)
+        btnUpd.setOnClickListener(this)
+        btnDel.setOnClickListener(this)
         btnClear.setOnClickListener(this)
         // создаем объект для создания и управления версиями БД
         dbHelper = DBHelper(this)
     }
 
-    override fun onClick(view: View) { // создаем объект для данных
+    override fun onClick(v: View) { // создаем объект для данных
         val contentValues = ContentValues()
         // получаем данные из полей ввода
         val name = etName.text.toString()
         val email = etEmail.text.toString()
+        val id = etID.text.toString()
         // подключаемся к БД
         val db = dbHelper!!.writableDatabase
-        when (view.id) {
+        when (v.id) {
             R.id.btnAdd -> {
                 Log.d(LOG_TAG, "--- Insert in mytable: ---")
                 // подготовим данные для вставки в виде пар: наименование столбца - значение
@@ -52,20 +54,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btnRead -> {
                 Log.d(LOG_TAG, "--- Rows in mytable: ---")
                 // делаем запрос всех данных из таблицы mytable, получаем Cursor
-                val cursor: Cursor = db.query("mytable", null, null, null, null, null, null)
+                val cursor = db.query("mytable", null, null, null, null, null, null)
                 // ставим позицию курсора на первую строку выборки
 // если в выборке нет строк, вернется false
                 if (cursor.moveToFirst()) { // определяем номера столбцов по имени в выборке
-                    val idColIndex: Int = cursor.getColumnIndex("id")
-                    val nameColIndex: Int = cursor.getColumnIndex("name")
-                    val emailColIndex: Int = cursor.getColumnIndex("email")
+                    val idColIndex = cursor.getColumnIndex("id")
+                    val nameColIndex = cursor.getColumnIndex("name")
+                    val emailColIndex = cursor.getColumnIndex("email")
                     do { // получаем значения по номерам столбцов и пишем все в лог
                         Log.d(LOG_TAG,
-                                "ID = " + cursor.getInt(idColIndex).toString() +
-                                        ", name = " + cursor.getString(nameColIndex).toString() +
-                                        ", email = " + cursor.getString(emailColIndex))
+                                "ID = " + cursor.getInt(idColIndex) + ", name = "
+                                        + cursor.getString(nameColIndex) + ", email = "
+                                        + cursor.getString(emailColIndex))
                         // переход на следующую строку
-// а если следующей нет (текущая - последняя), то false - выходим из цикла
+// а если следующей нет (текущая - последняя), то false -
+// выходим из цикла
                     } while (cursor.moveToNext())
                 } else Log.d(LOG_TAG, "0 rows")
                 cursor.close()
@@ -76,9 +79,30 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val clearCount = db.delete("mytable", null, null)
                 Log.d(LOG_TAG, "deleted rows count = $clearCount")
             }
+            R.id.btnUpd -> {
+                if (id.equals("", ignoreCase = true)) {
+                    return
+                }
+                Log.d(LOG_TAG, "--- Update mytable: ---")
+                // подготовим значения для обновления
+                contentValues.put("name", name)
+                contentValues.put("email", email)
+                // обновляем по id
+                val updCount = db.update("mytable", contentValues, "id = ?", arrayOf(id))
+                Log.d(LOG_TAG, "updated rows count = $updCount")
+            }
+            R.id.btnDel -> {
+                if (id.equals("", ignoreCase = true)) {
+                    return
+                }
+                Log.d(LOG_TAG, "--- Delete from mytable: ---")
+                // удаляем по id
+                val delCount = db.delete("mytable", "id = $id", null)
+                Log.d(LOG_TAG, "deleted rows count = $delCount")
+            }
         }
         // закрываем подключение к БД
-        dbHelper!!.close()
+        dbHelper?.close()
     }
 
     inner class DBHelper(context: Context?) : SQLiteOpenHelper(context, "myDB", null,
